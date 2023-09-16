199e5e86845691a42bbabd4dd3069b39a7d8299a
Faisal Arshad <faisalarshadciit@gmail.com>
2023-09-16 18:38:14 +0500

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Upwork-QRCode-Generator-Android-App"
+include(":app")
+ 
